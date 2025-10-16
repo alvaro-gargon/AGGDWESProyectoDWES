bb590b35ec4f04dc11d14ddf5b1a982917283132
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2025-10-16 12:27:29 +0200

    Upload to git

---
+++ indexProyectoDWES.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Álvaro García</title>
+    <link rel="stylesheet" href="./webroot/css/estilos.css"/>
+</head>
+<body>
+    <header>
+        <p>DESARROLLO DE APLICACIONES WEB EN EL LADO DEL SERVIDOR</p>
+    </header>
+    <div class="contenedor">
+        <div class="proyectos"><a target="_blank" href="./doc/AGG-DWES-EJ-U1.pdf">Estudio Tema1. DESARROLLO WEB EN ENTORNO SERVIDOR </a></div>
+        <div class="proyectos"><a target="_blank" href="./doc/README.html">Estudio Tema2. INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN </a></div>
+        <div class="proyectos"><a href="../AGGDWESProyectoTema3/indexProyectoTema3.php">Proyecto tema 3. CARACTERÍSTICAS DEL LENGUAJE PHP</a></div>
+        <div class="proyectos">Proyecto tema 4. TÉCNICAS DE ACCESO A DATOS EN PHP</div>
+        <div class="proyectos">Proyecto tema 5. DESARROLLO DE APLICACIONES WEB</div>
+        <div class="proyectos">Proyecto tema 6. APLICACIONES WEB MULTICAPA</div>
+        <div class="proyectos">Proyecto tema 7. PROGRAMACIÓN DE SERVICIOS WEB</div>
+        <div class="proyectos">Proyecto tema 8. DESARROLLO DE APLICACIONES WEB HÍBRIDAS</div>
+    </div>
+    <footer>
+        <p><a href="../">Álvaro García González</a></p>
+        
+        <p>Última actualización <time datetime="2025-10-03">03/10/2025</time></p>
+    </footer>
+</body>
+</html>
+
+
+
